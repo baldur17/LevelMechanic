20bb936dab12eaa56a9fc40c33a899a1b461ad90
unknown <baldur17@ru.is>
2020-02-20 00:23:22 +0000

    stuff

---
+++ Assets/Scripts/FaceMouse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class FaceMouse : MonoBehaviour
+{
+    private Camera _camera;
+    private bool _isCameraNotNull;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _isCameraNotNull = _camera != null;
+        _camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dir = Input.mousePosition - _camera.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
+   
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,191 +1,189 @@
-﻿using UnityEngine;
+﻿using AGDDPlatformer;
+using UnityEngine;
 
-namespace AGDDPlatformer
+public class PlayerController : KinematicObject
 {
-    public class PlayerController : KinematicObject
+    [Header("Movement")]
+    public float maxSpeed = 7;
+    public float jumpSpeed = 7;
+    public float jumpDeceleration = 0.5f; // Upwards slow after releasing jump button
+    public float coyoteTime = 0.1f; // Lets player jump just after leaving ground
+    public float jumpBufferTime = 0.1f; // Lets the player input a jump just before becoming grounded
+
+    [Header("Dash")]
+    public float dashSpeed;
+    public float dashTime;
+    public float dashCooldown;
+    public Color canDashColor;
+    public Color cantDashColor;
+    private float lastDashTime;
+    private Vector2 dashDirection;
+    private bool isDashing;
+    private bool canDash;
+    private bool wantsToDash;
+
+    [Header("Audio")]
+    public AudioSource source;
+    public AudioClip jumpSound;
+    public AudioClip dashSound;
+
+    private Vector2 startPosition;
+    private bool startOrientation;
+
+    private float lastJumpTime;
+    private float lastGroundedTime;
+    private bool canJump;
+    private bool jumpReleased;
+    private Vector2 move;
+    private float defaultGravityModifier;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
     {
-        [Header("Movement")]
-        public float maxSpeed = 7;
-        public float jumpSpeed = 7;
-        public float jumpDeceleration = 0.5f; // Upwards slow after releasing jump button
-        public float cayoteTime = 0.1f; // Lets player jump just after leaving ground
-        public float jumpBufferTime = 0.1f; // Lets the player input a jump just before becoming grounded
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
 
-        [Header("Dash")]
-        public float dashSpeed;
-        public float dashTime;
-        public float dashCooldown;
-        public Color canDashColor;
-        public Color cantDashColor;
-        float lastDashTime;
-        Vector2 dashDirection;
-        bool isDashing;
-        bool canDash;
-        bool wantsToDash;
+        lastJumpTime = -jumpBufferTime * 2;
 
-        [Header("Audio")]
-        public AudioSource source;
-        public AudioClip jumpSound;
-        public AudioClip dashSound;
+        startPosition = transform.position;
+        startOrientation = spriteRenderer.flipX;
 
-        Vector2 startPosition;
-        bool startOrientation;
+        defaultGravityModifier = gravityModifier;
+    }
 
-        float lastJumpTime;
-        float lastGroundedTime;
-        bool canJump;
-        bool jumpReleased;
-        Vector2 move;
-        float defaultGravityModifier;
+    void Update()
+    {
+        isFrozen = GameManager.instance.timeStopped;
 
-        SpriteRenderer spriteRenderer;
+        /* --- Read Input --- */
 
-        void Awake()
+        move.x = Input.GetAxisRaw("Horizontal");
+        if (gravityModifier < 0)
         {
-            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-
-            lastJumpTime = -jumpBufferTime * 2;
-
-            startPosition = transform.position;
-            startOrientation = spriteRenderer.flipX;
-
-            defaultGravityModifier = gravityModifier;
+            move.x *= -1;
         }
 
-        void Update()
+        move.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonDown("Jump"))
         {
-            isFrozen = GameManager.instance.timeStopped;
+            // Store jump time so that we can buffer the input
+            lastJumpTime = Time.time;
+        }
 
-            /* --- Read Input --- */
+        if (Input.GetButtonUp("Jump"))
+        {
+            jumpReleased = true;
+        }
 
-            move.x = Input.GetAxisRaw("Horizontal");
-            if (gravityModifier < 0)
+        // Clamp directional input to 8 directions for dash
+        Vector2 desiredDashDirection = new Vector2(
+            move.x == 0 ? 0 : (move.x > 0 ? 1 : -1),
+            move.y == 0 ? 0 : (move.y > 0 ? 1 : -1));
+        if (desiredDashDirection == Vector2.zero)
+        {
+            // Dash in facing direction if there is no directional input;
+            desiredDashDirection = spriteRenderer.flipX ? -Vector2.right : Vector2.right;
+        }
+        desiredDashDirection = desiredDashDirection.normalized;
+        if (Input.GetButtonDown("Dash"))
+        {
+            wantsToDash = true;
+        }
+
+        /* --- Compute Velocity --- */
+
+        if (canDash && wantsToDash)
+        {
+            isDashing = true;
+            dashDirection = desiredDashDirection;
+            lastDashTime = Time.time;
+            canDash = false;
+            gravityModifier = 0;
+
+            source.PlayOneShot(dashSound);
+        }
+        wantsToDash = false;
+
+        if (isDashing)
+        {
+            velocity = dashDirection * dashSpeed;
+            if (Time.time - lastDashTime >= dashTime)
             {
-                move.x *= -1;
-            }
-
-            move.y = Input.GetAxisRaw("Vertical");
-
-            if (Input.GetButtonDown("Jump"))
-            {
-                // Store jump time so that we can buffer the input
-                lastJumpTime = Time.time;
-            }
-
-            if (Input.GetButtonUp("Jump"))
-            {
-                jumpReleased = true;
-            }
-
-            // Clamp directional input to 8 directions for dash
-            Vector2 desiredDashDirection = new Vector2(
-                move.x == 0 ? 0 : (move.x > 0 ? 1 : -1),
-                move.y == 0 ? 0 : (move.y > 0 ? 1 : -1));
-            if (desiredDashDirection == Vector2.zero)
-            {
-                // Dash in facing direction if there is no directional input;
-                desiredDashDirection = spriteRenderer.flipX ? -Vector2.right : Vector2.right;
-            }
-            desiredDashDirection = desiredDashDirection.normalized;
-            if (Input.GetButtonDown("Dash"))
-            {
-                wantsToDash = true;
-            }
-
-            /* --- Compute Velocity --- */
-
-            if (canDash && wantsToDash)
-            {
-                isDashing = true;
-                dashDirection = desiredDashDirection;
-                lastDashTime = Time.time;
-                canDash = false;
-                gravityModifier = 0;
-
-                source.PlayOneShot(dashSound);
-            }
-            wantsToDash = false;
-
-            if (isDashing)
-            {
-                velocity = dashDirection * dashSpeed;
-                if (Time.time - lastDashTime >= dashTime)
-                {
-                    isDashing = false;
+                isDashing = false;
                     
-                    gravityModifier = defaultGravityModifier;
-                    if ((gravityModifier >= 0 && velocity.y > 0) ||
-                        (gravityModifier < 0 && velocity.y < 0))
-                    {
-                        velocity.y *= jumpDeceleration;
-                    }
+                gravityModifier = defaultGravityModifier;
+                if ((gravityModifier >= 0 && velocity.y > 0) ||
+                    (gravityModifier < 0 && velocity.y < 0))
+                {
+                    velocity.y *= jumpDeceleration;
                 }
             }
-            else
+        }
+        else
+        {
+            if (isGrounded)
             {
-                if (isGrounded)
-                {
-                    // Store grounded time to allow for late jumps
-                    lastGroundedTime = Time.time;
-                    canJump = true;
-                    if (!isDashing && Time.time - lastDashTime >= dashCooldown)
-                        canDash = true;
-                }
+                // Store grounded time to allow for late jumps
+                lastGroundedTime = Time.time;
+                canJump = true;
+                if (!isDashing && Time.time - lastDashTime >= dashCooldown)
+                    canDash = true;
+            }
 
-                // Check time for buffered jumps and late jumps
-                float timeSinceJumpInput = Time.time - lastJumpTime;
-                float timeSinceLastGrounded = Time.time - lastGroundedTime;
+            // Check time for buffered jumps and late jumps
+            float timeSinceJumpInput = Time.time - lastJumpTime;
+            float timeSinceLastGrounded = Time.time - lastGroundedTime;
 
-                if (canJump && timeSinceJumpInput <= jumpBufferTime && timeSinceLastGrounded <= cayoteTime)
-                {
-                    velocity.y = Mathf.Sign(gravityModifier) * jumpSpeed;
-                    canJump = false;
-                    isGrounded = false;
+            if (canJump && timeSinceJumpInput <= jumpBufferTime && timeSinceLastGrounded <= coyoteTime)
+            {
+                velocity.y = Mathf.Sign(gravityModifier) * jumpSpeed;
+                canJump = false;
+                isGrounded = false;
                     
-                    source.PlayOneShot(jumpSound);
-                }
-                else if (jumpReleased)
+                source.PlayOneShot(jumpSound);
+            }
+            else if (jumpReleased)
+            {
+                // Decelerate upwards velocity when jump button is released
+                if ((gravityModifier >= 0 && velocity.y > 0) ||
+                    (gravityModifier < 0 && velocity.y < 0))
                 {
-                    // Decelerate upwards velocity when jump button is released
-                    if ((gravityModifier >= 0 && velocity.y > 0) ||
-                        (gravityModifier < 0 && velocity.y < 0))
-                    {
-                        velocity.y *= jumpDeceleration;
-                    }
-                    jumpReleased = false;
+                    velocity.y *= jumpDeceleration;
                 }
-
-                velocity.x = move.x * maxSpeed;
+                jumpReleased = false;
             }
 
-            /* --- Adjust Sprite --- */
-
-            // Assume the sprite is facing right, flip it if moving left
-            if (move.x > 0.01f)
-            {
-                spriteRenderer.flipX = false;
-            }
-            else if (move.x < -0.01f)
-            {
-                spriteRenderer.flipX = true;
-            }
-
-            spriteRenderer.color = canDash ? canDashColor : cantDashColor;
+            velocity.x = move.x * maxSpeed;
         }
 
-        public void ResetPlayer()
+        /* --- Adjust Sprite --- */
+
+        // Assume the sprite is facing right, flip it if moving left
+        if (move.x > 0.01f)
         {
-            transform.position = startPosition;
-            spriteRenderer.flipX = startOrientation;
-
-            lastJumpTime = -jumpBufferTime * 2;
-
-            velocity = Vector2.zero;
+            spriteRenderer.flipX = false;
         }
-
-        public void ResetDash()
+        else if (move.x < -0.01f)
         {
-            canDash = true;
+            spriteRenderer.flipX = true;
         }
+
+        spriteRenderer.color = canDash ? canDashColor : cantDashColor;
+    }
+
+    public void ResetPlayer()
+    {
+        transform.position = startPosition;
+        spriteRenderer.flipX = startOrientation;
+
+        lastJumpTime = -jumpBufferTime * 2;
+
+        velocity = Vector2.zero;
+    }
+
+    public void ResetDash()
+    {
+        canDash = true;
     }
 }
