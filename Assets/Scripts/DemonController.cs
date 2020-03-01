d1d675a3cfd0e773073d85f8c9fde4e576b08819
unknown <baldur17@ru.is>
2020-03-01 13:38:49 +0000

    reykjavik

---
+++ Assets/Scripts/DemonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonController : MonoBehaviour
+{
+    public GameObject flamePrefab;
+
+    private float spawnInterval = 3f;
+
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > spawnInterval)
+        {
+            timer = 0f;
+            
+            var flame = Instantiate(flamePrefab);
+            flame.transform.position = gameObject.transform.position;
+        }
+    }
+}
