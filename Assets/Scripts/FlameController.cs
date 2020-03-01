f156c83ea7755fe82e6b2407d2df6e756291ad20
unknown <baldur17@ru.is>
2020-03-01 23:47:16 +0000

    stuff

--- Assets/Scripts/FlameController.cs
+++ Assets/Scripts/FlameController.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FlameController : MonoBehaviour
 {
     private const float Speed = 5;
+    public bool left = true;
 
     public string sceneName;
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.left * Speed;
+        if (left)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.left * Speed;
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.right * Speed;
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
     }
 
     // Update is called once per frame
